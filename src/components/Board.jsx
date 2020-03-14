327cc51d904d9af154700fc63652316fc79a1395
khinkle96 <khinkle0512@gmail.com>
2020-03-14 15:19:04 -0700

    fixed error

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import Row from 'react-bootstrap/Row'
-import Form from './Form'
 
 const Board = ()=> {
-
     return (
-        <section id="board">
-            <Row>
-                <Form title="Destination" />
-            </Row>
-        </section>
+        <div>
+        </div>
     )
 }
+    
 export default Board
