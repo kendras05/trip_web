a4698baa65fa209ede9c979fb7940ccfd59723f7
khinkle96 <khinkle0512@gmail.com>
2020-03-14 14:43:49 -0700

    Pulled from github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import logo from './assets/roadtrip.gif'
+
+const App = ()=> {
+
+import React from 'react';
+import './App.css';
+import Map from './leaflet.js';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="title" style = { {color: "white"} }>Trip Tracker</h1>
+      <p style={ {color: "white", fontSize: "14px"} }>Your new favorite trip planner 🚙</p>
+      <img src={logo}></img>
+      <Board />
+   
+			<Map />
+
+		</div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Board.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Form from './Form'
+
+const Board = ()=> {
+
+    return (
+        <section id="board">
+            <Row>
+                <Form title="Destination" />
+            </Row>
+        </section>
+    )
+}
+export default Board
